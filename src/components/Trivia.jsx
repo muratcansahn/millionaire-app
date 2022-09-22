a63ab2eafccd698c87aa8da38c63fabecfab50d8
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-09-22 15:29:40 +0300

    add questions

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Trivia = () => {
+  return (
+    <div className="trivia">
+      <div className="question">Are you oke</div>
+      <div className="answers">
+        <div className="answer">Lama Dev</div>
+        <div className="answer">Lama Dev</div>
+        <div className="answer">Lama Dev</div>
+        <div className="answer">Lama Dev</div>
+      </div>
+    </div>
+  );
+};
+
+export default Trivia;
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Pyramid } from "./Pyramid";
+export { default as Trivia } from "./Trivia";
