2b1e51278aa6c5b15a083a0a120c99932bf50bea
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-09-25 21:54:04 +0300

    add sound

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,10 +1,17 @@
 import { useState, useEffect } from "react";
+import useSound from "use-sound";
+import play from "../sounds/play.mp3";
+import correct from "../sounds/correct.mp3";
+import wrong from "../sounds/wrong.mp3";
 
 const Trivia = (props) => {
   const { data, setStop, setQuestionNumber, questionNumber } = props;
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState("answer");
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
 
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
@@ -17,11 +24,18 @@ const Trivia = (props) => {
     }, duration);
   };
 
+  // useEffect(() => {
+  //   letsPlay();
+  // }, [letsPlay]);
+
   const handleClick = (answer) => {
     setSelectedAnswer(answer);
     setClassName("answer active");
     delay(3000, () => {
       setClassName(answer.isCorrect ? "answer correct" : "answer wrong");
+      delay(2000, () => {
+        answer.isCorrect ? correctAnswer() : wrongAnswer();
+      });
       delay(6000, () => {
         if (answer.isCorrect) {
           setQuestionNumber((prev) => prev + 1);
